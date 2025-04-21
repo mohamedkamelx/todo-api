77cc5f59f48768c12b91f4ca78c4a4014cbe46d9
mohamedkamelx <167813862+mohamedkamelx@users.noreply.github.com>
2025-04-21 22:04:51 +0200

    Updated

--- mainpage/signals.py
+++ mainpage/signals.py
@@ -1,4 +1,3 @@
-# signals.py (create new file if needed)
 from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
